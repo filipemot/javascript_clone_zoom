1535576fad4ad56aeb76368f9e88cd3c5b7f4c65
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-01-26 17:37:30 -0300

    Change Aula 2

--- server/index.js
+++ server/index.js
@@ -1,35 +1,36 @@
 const server = require('http').createServer((request, response) => {
     response.writeHead(204, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
-    });
-    response.end('finish');
-});
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    })
+    response.end('hey there!')
+})
 
-const socketIo = require('socket.io');
-
+const socketIo = require('socket.io')
 const io = socketIo(server, {
     cors: {
         origin: '*',
-        credetials: false
+        credentials: false
     }
-});
+})
 
 io.on('connection', socket => {
-    console.log('connection', socket.id);
+    console.log('connection', socket.id)
     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
+        
+        // adiciona os usuarios na mesma sala
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
         socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId);
-        });
-    });
-});
+            console.log('disconnected!', roomId, userId)
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
 
 const startServer = () => {
-    const { address, port } = server.address();
-    console.info(`app running at ${address}: ${port}`);
+    const { address, port } = server.address()
+    console.info(`app running at ${address}:${port}`)
 }
 
-server.listen(process.env.PORT || 3000, startServer);
-
+server.listen(process.env.PORT || 3000, startServer)
